d4513825f9a4ddd37d888d586658c33b2dbdf02b
Harshit <harshityadav.0025@gmail.com>
2024-06-07 20:43:36 +0530

    Adding carousel to home page only because we need search functionality, also implementing search functionality

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -5,6 +5,7 @@ import Card from "../Components/Card";
 import Carousel from "../Components/Carousel";
 
 export default function Home() {
+    const [search, setSearch] = useState("");
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
@@ -33,7 +34,80 @@ export default function Home() {
         <Navbar />
       </div>
       <div>
-        <Carousel />
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade data-bs-ride"
+          style={{ objectFit: "cover !important" }}
+        >
+          <div className="carousel-inner" id="carousel">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value = {search}
+                  onChange={(e)=>{setSearch(e.target.value)}}
+                />
+                {/* <button
+                  className="btn btn-outline-success text-white bg-success"
+                  type="submit"
+                >
+                  Search
+                </button> */}
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/random/900×700/?burger"
+                className="d-block w-100 "
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?pizza"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?barbeque"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
       <div className="container">
         {foodCat != []
@@ -46,14 +120,17 @@ export default function Home() {
                   <hr />
                   {foodItem != [] ? (
                     foodItem
-                      .filter((item) => item.CategoryName === data.CategoryName)
+                      .filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLowerCase())))
                       .map((filterItems) => {
                         return (
-                          <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
-                            <Card 
-                                foodName = {filterItems.name} 
-                                options = {filterItems.options[0]}
-                                imgsrc = {filterItems.img}
+                          <div
+                            key={filterItems._id}
+                            className="col-12 col-md-6 col-lg-3"
+                          >
+                            <Card
+                              foodName={filterItems.name}
+                              options={filterItems.options[0]}
+                              imgsrc={filterItems.img}
                             />
                           </div>
                         );
